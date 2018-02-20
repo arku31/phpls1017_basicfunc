716f65ff0a6189e47aeea887322dcc0b5d267544
Igor Tverdokhleb <itvrd2@yandex.ru>
2018-02-20 21:21:08 +0300

    Привел к стандарту PSR-2

--- functions/index.php
+++ functions/index.php
@@ -1,21 +1,28 @@
 <?php
-function getX() {
+function getX()
+{
     $x = 'xxxxxx';
     return $x;
 }
 
-function getY() {
+function getY()
+{
     $y = 'yyyy';
     echo $y;
 }
 
 $x = getX();
-function get_post() {
+function get_post()
+{
+    //pre-intermediante, A2
     return 'post';
 }
 
-function the_post() {
+function the_post()
+{
     echo get_post();
 }
+//Ctrl+alt+L
+//PSR-2
 
 
